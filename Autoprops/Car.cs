90e8a71bdf2b15fc23c1434f6fd09079b3f86ff0
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 14:03:31 -0400

    Add body to Main method

--- Autoprops/Car.cs
+++ Autoprops/Car.cs
@@ -11,5 +11,12 @@ namespace Autoprops
         public string PetName { get; set; }
         public int Speed { get; set; }
         public string Color { get; set; }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Car Name: {0}", PetName);
+            Console.WriteLine("Speed: {0}", Speed);
+            Console.WriteLine("Color: {0}", Color);
+        }
     }
 }
--- Autoprops/Program.cs
+++ Autoprops/Program.cs
@@ -10,6 +10,17 @@ namespace Autoprops
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Automatic Properties *****\n");
+
+            Car c = new Car();
+            c.PetName = "Frank";
+            c.Speed = 55;
+            c.Color = "Red";
+
+            Console.WriteLine("Your car is named {0}? That's odd...", c.PetName);
+            c.DisplayStats();
+
+            Console.ReadLine();
         }
     }
 }
